4cad5e50943c8216afff964d7de7f72f08a8fa88
IsaiasQB <isaias.qb@hotmail.com>
2022-06-14 18:59:16 -0400

    created the functionality for comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 //Schema defining the fields with specific data types, using the Schema constructor imported. 
 const PizzaSchema = new Schema ({
@@ -10,14 +11,37 @@ const PizzaSchema = new Schema ({
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
   },
   size: {
     type: String,
     default: 'Large'
   },
-  toppings: []
+  toppings: [],
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
+},
+//tell the schema that it can use virtuals and getters.
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+
+//get total count of comments and replies on retrieval
+PizzaSchema.virtual('CommentCount').get(function(){
+  return this.comments.lenght;
 });
+
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
